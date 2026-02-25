c33057e7da491c5b7779a8f452cb2d3b63015f7b
qkzhwls <qkzhwls1@gmail.com>
2026-02-25 12:04:33 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,14 +4,14 @@ import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/fireb
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyBxmX7fEISWYs_JGktAZrFjdb8cb_ZcmSY",
-    authDomain: "work-tool-e2943.firebaseapp.com",
-    projectId: "work-tool-e2943",
-    storageBucket: "work-tool-e2943.appspot.com",
-    messagingSenderId: "133294945093",
-    appId: "1:133294945093:web:cde90aab6716127512842c",
-    measurementId: "G-ZZQLKB0057"
-};
+    apiKey: "AIzaSyAguJOtoqoSipA-wXH3jSYX2yH1RX7tQQw",
+    authDomain: "location-e2ff9.firebaseapp.com",
+    projectId: "location-e2ff9",
+    storageBucket: "location-e2ff9.firebasestorage.app",
+    messagingSenderId: "559399838918",
+    appId: "1:559399838918:web:91c3bbf98adb92d2a863c7",
+    measurementId: "G-RTBSE9SN1Q"
+  };
 
 const APP_ID = 'team-work-logger-v2';
 let db, auth;
@@ -172,4 +172,5 @@ function getDefaultConfig() {
              weekend: 4
         }
     };
+
 }
